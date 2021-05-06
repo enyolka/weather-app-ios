10d645fd9295fc4852d84f35c7a19b0c1ea75ad2
Użytkownik Gość <Guest@iMac-AppleLab-3.local>
2021-05-06 09:47:12 +0200

    initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Użytkownik Gość on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
